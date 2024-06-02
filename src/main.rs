8f472480cf695008ad983de549d2eff86cd81ef6
Vid Drobnič <vid.drobnic@pm.me>
2024-06-02 20:28:21 +0200

    feat: basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{fs, path::PathBuf};
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Path of the file to run
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    run(cli.path);
+}
+
+fn run(path: PathBuf) {
+    let input = match fs::read_to_string(path) {
+        Ok(input) => input,
+        Err(err) => panic!("Failed to read input file: {err}"),
+    };
+
+    // TODO: Better error handling
+    let program = parser::parse(&input).unwrap();
+    runtime::run(&program).unwrap();
 }
